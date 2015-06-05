32935fd583420de42662ab69d76e5bd3e5402571
roxanapintos <roxypin@ipcorp.com.ar>
2015-06-05 19:56:41 -0300

    Primer commit by A.S.

---
+++ app/src/main/java/com/productos/detalle/roxana/detalleproductos/DetalleProductos.java
@@ -0,0 +1,9 @@
+package com.productos.detalle.roxana.detalleproductos;
+
+import android.app.Activity;
+
+/**
+ * Created by roxana on 05/06/15.
+ */
+public class DetalleProductos extends Activity {
+}
